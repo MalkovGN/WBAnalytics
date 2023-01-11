6a6aaab803d2bb4cce3534b8c5f297b323e7ce53
MalkovGN <malkovgn@gmail.com>
2023-01-11 23:28:18 +0300

    Add collecting info functions

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from bs4 import BeautifulSoup as Soup
 from json_parser import JsonParser
 from check_proxy import LOGIN, PASSWORD
 
@@ -35,12 +34,81 @@ def collect_all_urls():
         test_json.start()
         all_urls += test_json.subcategory_info
 
-    print(all_urls)
-    print(len(all_urls))
+    # print(all_urls)
+    # print(len(all_urls))
     return all_urls
+
+
+def get_categories_without_shard(subcategories_info):
+    """
+    :param subcategories_info: JSON with all subcategories
+    :return: The list with subcategories which has not
+    shard and query params
+    """
+    categories_without_shard = []
+
+    for subcategory in subcategories_info:
+        if 'shard' not in subcategory.keys():
+            categories_without_shard.append(subcategory)
+
+    # print(categories_without_shard)
+    # print(len(categories_without_shard))
+    return categories_without_shard
+
+
+def get_full_info_categories(subcategories_info):
+    """
+    :param subcategories_info: JSON with all subcategories
+    :return: The list only with shard and query params
+    """
+    full_info_subcategories = []
+
+    for subcategory in subcategories_info:
+        if 'shard' not in subcategory.keys():
+            continue
+        else:
+            full_info_subcategories.append(subcategory)
+    # print(full_info_subcategories)
+    # print(len(full_info_subcategories))
+    return full_info_subcategories
+
+
+def number_of_products_links(subcategories_info):
+    """
+    Creating list of urls to check
+    number of products in subcategory
+    :param subcategories_info: JSON with
+     subcategories params
+    :return: The list of urls
+    """
+    urls_list = []
+    for subcategory in subcategories_info:
+        shard = subcategory['shard']
+        query = subcategory['query']
+        split_query = query.split('&')
+        if split_query[0][:4] == 'kind':
+            kind_query = split_query[0]
+            subject_query = split_query[1]
+        elif split_query[0][:4] == 'subj':
+            kind_query = ''
+            subject_query = split_query[0]
+        else:
+            continue
+
+        url = f'https://catalog.wb.ru/catalog/{shard}/v4/filters?appType=1&couponsGeo=12,3,18,15,21&curr=rub&dest=-1029256,-102269,-2162196,-1257786&emp=0&{kind_query}&lang=ru&locale=ru&pricemarginCoeff=1.0&reg=0&regions=80,64,83,4,38,33,70,68,69,86,75,30,40,48,1,66,31,22,71&spp=0&{subject_query}'
+        urls_list.append(url)
+
+    # print(urls_list)
+    # print(len(urls_list))
+    return urls_list
 
 
 if __name__ == '__main__':
     URL = 'https://static-basket-01.wb.ru/vol0/data/main-menu-ru-ru.json'
-    collect_main_page_data(URL)
-    collect_all_urls()
+    # collect_main_page_data(URL)
+    # collect_all_urls()
+    subcategories_info = collect_all_urls()
+    # get_categories_without_shard(subcategories_info)
+    s1 = get_full_info_categories(subcategories_info)
+    number_of_products_links(s1)
+    # number_of_products_links()
