4ef5fad23f6ecd0f60beaca5afc094a3eaacc375
MalkovGN <malkovgn@gmail.com>
2023-01-12 00:37:44 +0300

    Create a number of products urls model

--- wb_analytic/wb_analytic_app/models.py
+++ wb_analytic/wb_analytic_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductsNumberUrlsModel(models.Model):
+    category = models.CharField(max_length=128)
+    url = models.TextField()
+
+    def __str__(self):
+        return self.category
