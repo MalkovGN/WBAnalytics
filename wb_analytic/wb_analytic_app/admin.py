441247bb8fd23d09107d9b41afe9e1b84c6af851
MalkovGN <malkovgn@gmail.com>
2023-01-18 15:00:07 +0300

    Register a new model

--- wb_analytic/wb_analytic_app/admin.py
+++ wb_analytic/wb_analytic_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import ProductsNumberUrlsModel, ProductsInCategory, ProductsNotSaved
+from .models import ProductsNumberUrlsModel, ProductsInCategory, ProductsNotSaved, CategoryPageInfo
 
 
 admin.site.register(ProductsNumberUrlsModel)
@@ -8,3 +8,5 @@ admin.site.register(ProductsNumberUrlsModel)
 admin.site.register(ProductsInCategory)
 
 admin.site.register(ProductsNotSaved)
+
+admin.site.register(CategoryPageInfo)
