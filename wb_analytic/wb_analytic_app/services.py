fdf1f684b1396ab50b0a9e4d75bf7ebc9f710ebf
MalkovGN <malkovgn@gmail.com>
2023-01-22 18:05:41 +0300

    Adding HTML logic

--- wb_analytic/wb_analytic_app/services.py
+++ wb_analytic/wb_analytic_app/services.py
@@ -1,5 +1,11 @@
 import json
 import os
+import requests
+import random
+import time
+
+from wb_analytic_app.models import CategoryPageInfo, CategoriesNotSaved, ProductInfo
+from wb_analytic_app.file import proxies, times_sleeps
 
 
 class JsonParser:
@@ -155,3 +161,74 @@ def get_categories_without_shard(subcategories_info):
     # print(categories_without_shard)
     # print(len(categories_without_shard))
     return categories_without_shard
+
+
+def collect_page_info():
+    """
+    Saving to database
+    1) Category name
+    2) wb_url
+    3) url to first page products info
+    """
+    subcategories_info = collect_all_urls()
+    s1 = get_full_info_categories(subcategories_info)
+
+    for elem in s1:
+        name = elem['name']
+        wb_url = elem['url']
+        shard = elem['shard']
+        query = elem['query']
+        split_query = query.split('&')
+        page_number = 1  # When I will know how many pages
+        # I need to get -- CHANGE IT
+        if split_query[0][:4] == 'kind':
+            kind_query = split_query[0]
+            subject_query = split_query[1]
+        elif split_query[0][:4] == 'subj':
+            kind_query = ''
+            subject_query = query
+        else:
+            continue
+        first_page_products_url = f'https://catalog.wb.ru/catalog/{shard}/catalog?appType=1&couponsGeo=12,3,18,15,21&curr=rub&dest=-1029256,-102269,-2162196,-1257786&emp=0&{kind_query}&lang=ru&locale=ru&page={page_number}&pricemarginCoeff=1.0&reg=0&regions=80,64,83,4,38,33,70,68,69,86,75,30,40,48,1,66,31,22,71&spp=0&{subject_query}'
+
+        page_info = CategoryPageInfo(
+            name=name,
+            wb_url=wb_url,
+            first_page_products_url=first_page_products_url,
+        )
+        page_info.save()
+
+
+def collect_product_info():
+    categories = CategoryPageInfo.objects.all()
+
+    for category in categories:
+        url = category.first_page_products_url
+        # print(url)
+        proxy = random.choice(proxies)
+        # print(proxy)
+        time.sleep(random.choice(times_sleeps))
+
+        try:
+            response = requests.get(url=url, proxies=proxy)
+            time.sleep(random.choice(times_sleeps))
+            products = response.json()['data']['products']
+            for product in products:
+                # print(product['id'], product['name'], product['feedbacks'], product['priceU'], product['salePriceU'], '\n')
+                product_info = ProductInfo(
+                    product_id=product['id'],
+                    name=product['name'],
+                    feedbacks=product['feedbacks'],
+                    sale_price=product['salePriceU'],
+                    price=product['priceU'],
+                    category_id=category.pk,
+                )
+                product_info.save()
+
+        except Exception:
+            not_saved_category = CategoriesNotSaved(
+                name=category.name,
+                wb_url=category.wb_url,
+                first_page_products_url=category.first_page_products_url,
+            )
+            not_saved_category.save()
