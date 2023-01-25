5f143fad5c0f701cebe693708b86b5d9648cfb4e
MalkovGN <malkovgn@gmail.com>
2023-01-25 18:26:35 +0300

    Function to get amount of sold products

---
+++ wb_analytic/wb_analytic_app/management/commands/collecting_sold_number.py
@@ -0,0 +1,45 @@
+import random
+import time
+import requests
+
+from django.core.management.base import BaseCommand
+
+from wb_analytic_app.management.commands.collect_prod_nums import proxies, times_sleeps
+from wb_analytic_app.models import ProductInfo, AmountProductsNotSaved
+
+
+def collecting_sold_numbers():
+    """
+    Collecting amount of sold
+    products
+    """
+    time_start = time.time()
+    products = ProductInfo.objects.all()
+
+    for product in products:
+        vendor_code = product.product_id
+        proxy = random.choice(proxies)
+        time.sleep(random.choice(times_sleeps))
+        url = f'https://product-order-qnt.wildberries.ru/by-nm/?nm={vendor_code}'
+
+        try:
+            response = requests.get(url=url, proxies=proxy)
+            qnt = response.json()[0]['qnt']
+            product_amount = ProductInfo(
+                sold_number=qnt,
+            )
+            product_amount.save()
+        except Exception:
+            product_amount_not_saved = AmountProductsNotSaved(
+                product_id=product.product_id,
+                name=product.name,
+                category_id=product.category_id,
+            )
+            product_amount_not_saved.save()
+            print(f'Product {product.name} didnt save!!!')
+    print(f'The program was executed {time.time() - time_start} s.')
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        collecting_sold_numbers()
