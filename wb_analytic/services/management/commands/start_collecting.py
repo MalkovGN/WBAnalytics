f0f625f6f2c98bb43102a921d9b1094b12739aef
MalkovGN <malkovgn@gmail.com>
2023-03-15 18:02:30 +0300

    Uncomment collecting functions

--- wb_analytic/services/management/commands/start_collecting.py
+++ wb_analytic/services/management/commands/start_collecting.py
@@ -5,7 +5,7 @@ from services import collecting_scripts, secondary_scripts
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # collecting_scripts.collect_main_page_data()
-        # collecting_scripts.collect_category_name_id()
-        # collecting_scripts.collect_page_info()
+        collecting_scripts.collect_main_page_data()
+        collecting_scripts.collect_category_name_id()
+        collecting_scripts.collect_page_info()
         collecting_scripts.collect_product_info()
