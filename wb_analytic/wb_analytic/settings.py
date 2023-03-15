caf5ae3e2817f970878f3463087d31ee496ea1f8
MalkovGN <malkovgn@gmail.com>
2023-03-15 18:08:01 +0300

    Change db settings

--- wb_analytic/wb_analytic/settings.py
+++ wb_analytic/wb_analytic/settings.py
@@ -77,15 +77,15 @@ WSGI_APPLICATION = 'wb_analytic.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'wb_analytic',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'accesspassword',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'wb_analytic',
-        'USER': 'postgres',
-        'PASSWORD': 'accesspassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-    'main_server_db': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'wb_db',
         'USER': 'nikolai',
